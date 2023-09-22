bf31d36fefd9b6f4302bd5bd57ba2080b291596d
AaronMathew11 <72664456+AaronMathew11@users.noreply.github.com>
2023-09-22 14:55:51 +0530

    initial commit

--- src/Pages/About-page/about-page.jsx
+++ src/Pages/About-page/about-page.jsx
@@ -1,129 +1,267 @@
-import React from 'react'
-import "./about-page.css"
+import React from "react";
+import "./about-page.css";
+import logo from "../../Images/Iothinc-logo.png";
+import image from "../../Images/dots.png";
+
 function About() {
   return (
-    <div>
-    <div class="box-1">
-        <ul>
-            <li id="first">Meet The people behind</li>
-            <li id="second">IOT Gamified</li>
-        </ul>
-    </div>
-    <div class="box2" id="first_part">
-        <ul>
-            <li>
-                <img src="images-p/final_iot_logo.png" alt="" id="sec1"/>
-            </li>
-            <li>
-                <p id="sec2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae iste repellat dolorum
-                    quis esse laboriosam tenetur adipisci, expedita dolore eum numquam rem accusantium, distinctio
-                    tempore. Iusto quaerat corrupti aut repellendus sunt omnis soluta, iste ab enim. Quos at rerum
-                    corporis eveniet impedit, voluptate nulla eligendi officiis voluptas blanditiis, numquam minima,
-                    facilis sapiente porro odio quas iure dolores eaque ratione consectetur optio dolorum ab maxime
-                    laborum. Quidem minima libero ea neque dolorem magni maiores, nisi est fugiat accusamus enim beatae,
-                    nobis quod dolore, omnis nostrum tempora vel consectetur eaque ratione sequi! Asperiores facilis
-                    ducimus tenetur cumque illo optio exercitationem veritatis perspiciatis, in itaque necessitatibus
-                    odit harum pariatur quasi mollitia deleniti nisi repellat aliquam voluptatum nulla at. Ipsam quidem
-                    quaerat reprehenderit vero illo officiis soluta veniam impedit laboriosam, atque ratione totam quam
-                    necessitatibus neque repellendus saepe et! Placeat veritatis reprehenderit assumenda eaque porro
-                    consequatur! Similique animi culpa sunt eius sequi iure incidunt!</p>
-            </li>
-        </ul>
-    </div>
-    <div class="mid">
-        <div class="part1">
-
-            <p id="design">
-                DESIGNER
+    <div style={{ padding: "40px",  }}>
+      <div
+        style={{
+          marginTop: "120px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ width: "70%" }}>
+          <h1 style={{ marginBottom: 50 }}>
+            Meet the people behind IOT Gamified
+          </h1>
+          <div className='content-container'>
+            <img src={logo} height={200} width={400} id='page-logo' />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua.
             </p>
-            <div class="member-1">
-                <ul>
-                    <li>
-                        <div id="pic-1"></div>
-                        <p>Jury 1</p>
-                    </li>
-                    <li>
-                        <div id="pic-2"></div>
-                        <p>Jury 2</p>
-                    </li>
-                    <li>
-                        <div id="pic-3"></div>
-                        <p>Jury 3</p>
-                    </li>
-                    <li>
-                        <div id="pic-4"></div>
-                        <p>Jury 4</p>
-                    </li>
-                    <li>
-                        <div id="pic-5"></div>
-                        <p>Jury 5</p>
-                    </li>
-                </ul>
-            </div>
+          </div>
         </div>
-        <div class="part2">
 
-            <p id="Developer">
-                DEVELOPERS
-            </p>
-            <div class="member-2">
-                <ul>
-                    <li>
-                        <div id="pic-21"></div>
-                        <p>Jury 1</p>
-                    </li>
-                    <li>
-                        <div id="pic-22"></div>
-                        <p>Jury 2</p>
-                    </li>
-                    <li>
-                        <div id="pic-23"></div>
-                        <p>Jury 3</p>
-                    </li>
-                    <li>
-                        <div id="pic-24"></div>
-                        <p>Jury 4</p>
-                    </li>
-                    <li>
-                        <div id="pic-25"></div>
-                        <p>Jury 5</p>
-                    </li>
-                </ul>
+        <div style={{ width: "70%" }}>
+          <h1 style={{ marginTop: 60, marginBottom: 60, textAlign: "left" }}>
+            Designers
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
             </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+          </div>
         </div>
-        <div class="part3">
 
-            <p id="Mng">
-                Management team
-            </p>
-            <div class="member-3">
-                <ul>
-                    <li>
-                        <div id="pic-31"></div>
-                        <p>Jury 1</p>
-                    </li>
-                    <li>
-                        <div id="pic-32"></div>
-                        <p>Jury 2</p>
-                    </li>
-                    <li>
-                        <div id="pic-33"></div>
-                        <p>Jury 3</p>
-                    </li>
-                    <li>
-                        <div id="pic-34"></div>
-                        <p>Jury 4</p>
-                    </li>
-                    <li>
-                        <div id="pic-35"></div>
-                        <p>Jury 5</p>
-                    </li>
-                </ul>
+        <div style={{ width: "70%" }}>
+          <h1 style={{ marginTop: 80, marginBottom: 60, textAlign: "left" }}>
+            Developers
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
             </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+          </div>
         </div>
+
+        <div style={{ width: "70%" }}>
+          <h1 style={{ marginTop: 80, marginBottom: 60, textAlign: "left" }}>
+            Management Team
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+              flexWrap: "wrap",
+              justifyContent: "center",
+              marginBottom: 160,
+            }}
+          >
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+            <div className='team-member'>
+              <div
+                style={{
+                  backgroundColor: "black",
+                  height: 200,
+                  width: 200,
+                  borderRadius: 900,
+                  marginBottom: 20,
+                }}
+              ></div>
+              <p>Name here</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
 export default About;
