56ae2356cebee8ea4d8a4e93620b4bf93781cbe0
Ebrahim Mamawala <ebrahimmamawala@gmail.com>
2023-09-22 18:46:56 +0530

    Update timeline

--- src/Pages/Timeline/Timeline.jsx
+++ src/Pages/Timeline/Timeline.jsx
@@ -5,10 +5,30 @@ import "./stylescopy.css";
 
 function Timeline() {
 
-  const [sub1,setSub1] = useState();
-  const [sub2, setSub2] = useState();
-  const [sub3, setSub3] = useState();
+  const [sub1, setSub1] = useState(true);
+  const [sub2, setSub2] = useState(false);
+  const [sub3, setSub3] = useState(false);
 
+  function onClickSub1()
+  {
+    setSub1(true);
+    setSub2(false);
+    setSub3(false);
+  };
+
+  function onClickSub2()
+  {
+    setSub1(false);
+    setSub2(true);
+    setSub3(false);
+  };
+
+  function onClickSub3()
+  {
+    setSub1(false);
+    setSub2(false);
+    setSub3(true);
+  };
 
   return (
     <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
@@ -16,23 +36,34 @@ function Timeline() {
       <div style={{display:"flex", flexDirection:'row',marginTop:"120px", padding:40, alignItems:'center', justifyContent:'center', gap:180, }} className="content-container">
         <div style={{display:"flex" , flexDirection:"column", alignItems:"center"}}>
           <div style={{display:"flex",}}>
-          <div style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex", marginRight:30}}>Sub1</div>
-          <div style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex"}}>Sub2</div>
+          <div onClick={onClickSub1} style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex", marginRight:30}}>Sub1</div>
+          <div onClick={onClickSub2} style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex"}}>Sub2</div></div>
+          <div onClick={onClickSub3} style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex", marginTop:30}}>Sub3</div>
+        </div>
+        {
+          sub1 && <div>
+            <h2>Heading 1</h2>
+            <p>
+              This is supposed to be a long paragraph 1.
+            </p>
           </div>
-          <div style={{height:"100px",borderRadius:"20px", width:"100px", backgroundColor:"#15191F", padding:"20px", alignItems:'center', justifyContent:"center", display:"flex", marginTop:30}}>Sub3</div>
-        </div>
-        <div>
-          <h2>Heading</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
+        }
+        {
+          sub2 && <div>
+            <h2>Heading 2</h2>
+            <p>
+            This is supposed to be a long paragraph 2.
+            </p>
+          </div>
+        }
+        {
+          sub3 && <div>
+            <h2>Heading 3</h2>
+            <p>
+            This is supposed to be a long paragraph 3.
+            </p>
+          </div>
+        }
       </div>
       <h1 style={{paddingLeft:"40px", marginTop:80}}>TimeLine</h1>
       <section class='section-timeline'>
